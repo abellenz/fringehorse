d18dc786dc450772e64de702cd1c7e212cb5ed7e
abellenz <abellenz@gmail.com>
2015-12-29 22:47:19 -0500

    Hail Satan

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('prototypehorse')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('prototypehorse', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
