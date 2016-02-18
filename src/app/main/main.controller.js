3eb42b77fde52ffabe88e6074fb4953cef9b9f3d
abellenz <abellenz@gmail.com>
2016-02-18 10:06:30 -0500

    did proper sizing, need resize window event now, fuck jquery

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,6 +9,20 @@
   function MainController($timeout, $log, webDevTec) {
 
     $log.info('I hate you');
+    $(function() {
+      var $youTubeIframes = $('.youtubeIframe');
+      var fuck = $('#fuck');
+      console.log(fuck);
+      $youTubeIframes.each( function(index, value) {
+        var width = $(this).width();
+        $(this).css('height', (0.5625 * width));
+      } );
+
+      // for (i=0; i < youTubeIframes.length; i++) {
+      //   var width = youTubeIframes[i].width();
+      //   youTubeIframes[i].css('height', (0.5625 * width));
+      // }
+    });
 
   }
 })();
