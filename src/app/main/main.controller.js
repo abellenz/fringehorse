4cb0808a9357d723e7b9273d169c398d80536e42
abellenz <abellenz@gmail.com>
2016-02-18 19:41:21 -0500

    new favicon, title, links

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,23 +6,23 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, $log, webDevTec) {
+  function MainController($scope, $timeout, $log, $window, webDevTec) {
 
     $log.info('I hate you');
-    $(function() {
-      var $youTubeIframes = $('.youtubeIframe');
-      var fuck = $('#fuck');
-      console.log(fuck);
-      $youTubeIframes.each( function(index, value) {
-        var width = $(this).width();
-        $(this).css('height', (0.5625 * width));
-      } );
-
-      // for (i=0; i < youTubeIframes.length; i++) {
-      //   var width = youTubeIframes[i].width();
-      //   youTubeIframes[i].css('height', (0.5625 * width));
-      // }
+    var reFuckinSize = function() {
+      $(function() {
+        var $youTubeIframes = $('.youtubeIframe');
+        $youTubeIframes.each( function(index, value) {
+          var width = $(this).width();
+          $(this).css('height', (0.5625 * width));
+        });
+      });
+    };
+    reFuckinSize();
+    $(window).resize(function(){
+      $scope.$apply(function(){
+        reFuckinSize();
+      });
     });
-
   }
 })();
