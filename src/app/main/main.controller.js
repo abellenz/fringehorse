f88359dc83ff5c92ec4cf1d50bcf24cbc8cd56c7
abellenz <abellenz@gmail.com>
2016-02-22 20:21:50 -0500

    progress on layout

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -12,13 +12,28 @@
     var reFuckinSize = function() {
       $(function() {
         var $youTubeIframes = $('.youtubeIframe');
+        var $mobileIframes = $('.mobileIframe');
         $youTubeIframes.each( function(index, value) {
           var width = $(this).width();
           $(this).css('height', (0.5625 * width));
         });
+        $mobileIframes.each( function(index, value) {
+          var width = $(this).width();
+          $(this).css('height', (0.5625 * width));
+        });
       });
+      $log.info('fuck you');
     };
-    reFuckinSize();
+
+    var runResizer = function() {
+      $timeout(function(){
+        $log.info('this is awful');
+        reFuckinSize();
+        runResizer();
+      }, 1000);
+    };
+    runResizer();
+
     $(window).resize(function(){
       $scope.$apply(function(){
         reFuckinSize();
