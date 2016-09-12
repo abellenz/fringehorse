b54a73670ddcf8650d5aa492924f83e23632da70
abellenz <abellenz@gmail.com>
2016-09-12 17:15:46 -0400

    got lightbox gallery working

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('prototypehorse', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+    .module('prototypehorse', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr', 'bootstrapLightbox']);
 
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,9 +1,21 @@
 (function() {
   'use strict';
 
+  // don't even think about ading dependencies here - instead use index.module.js - fuck you
   angular
-    .module('prototypehorse')
-    .controller('MainController', MainController);
+    .module('prototypehorse' )
+    .controller('MainController', MainController)
+    .controller('GalleryCtrl', function ($scope, Lightbox) {
+      $scope.images = [
+        {
+          'url': 'https://upload.wikimedia.org/wikipedia/commons/0/07/Kamp_Alexisdorf_3.jpg',
+          'thumbUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Kamp_Alexisdorf_3.jpg/120px-Kamp_Alexisdorf_3.jpg'
+        },
+
+      ];
+
+      $scope.Lightbox = Lightbox;
+    });
 
   /** @ngInject */
   function MainController($scope, $timeout, $log, $window) {
@@ -11,55 +23,8 @@
     $scope.grid = false;
 
     $log.info('I hate you');
-    var reFuckinSize = function() {
-      $(function() {
-        var $youTubeIframes = $('.youtubeIframe');
-        var $mobileIframes = $('.mobileIframe');
-        var $fuckHorses = $('.fuckHorse');
-        var $smallFuckHorses = $('.smallFuckHorse');
-        var $smallFuckHorseTalls = $('.smallFuckHorseTall');
-        var $tinyFuckHorses = $('.tinyFuckHorse');
-        $youTubeIframes.each( function(index, value) {
-          var width = $(this).width();
-          $(this).css('height', (0.5625 * width));
-        });
-        $mobileIframes.each( function(index, value) {
-          var width = $(this).width();
-          $(this).css('height', (0.5625 * width));
-        });
-        $fuckHorses.each( function(index, value) {
-          var width = $(this).width();
-          $(this).css('height', (0.5625 * width));
-        });
-        $smallFuckHorses.each( function(index, value) {
-          var width = $(this).width();
-          $(this).css('height', (0.75 * width));
-        });
-        $smallFuckHorseTalls.each( function(index, value) {
-          var width = $(this).width();
-          $(this).css('height', (0.90 * width));
-        });
-        $tinyFuckHorses.each( function(index, value) {
-          var width = $(this).width();
-          $(this).css('height', (0.75 * width));
-        });
-      });
-      $log.info('fuck you');
-    };
 
-    var runResizer = function() {
-      $timeout(function(){
-        $log.info('this is awful');
-        reFuckinSize();
-        runResizer();
-      }, 3000);
-    };
-    runResizer();
+    $log.info('fuck you');
 
-    $(window).resize(function(){
-      $scope.$apply(function(){
-        reFuckinSize();
-      });
-    });
   }
 })();
